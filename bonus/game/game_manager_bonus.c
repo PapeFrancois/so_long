a1e1e3f4adef5aa6bb39243a290f76dd518d7fa6
Heloise POMPIDOU <heloise.pompidou@outlook.fr>
2023-10-13 14:57:22 +0200

    ajout d'un message si la map est trop grands

--- bonus/game/game_manager_bonus.c
+++ bonus/game/game_manager_bonus.c
@@ -6,7 +6,7 @@
 /*   By: hepompid <hepompid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 12:55:14 by hepompid          #+#    #+#             */
-/*   Updated: 2023/10/13 12:29:57 by hepompid         ###   ########.fr       */
+/*   Updated: 2023/10/13 14:30:56 by hepompid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,10 @@ void	game_manager(char **map)
 	game.mlx = mlx_init();
 	map_dimension_finder(map, &game.y, &game.x);
 	if (game.x > 1920 || game.y > 1080)
+	{
+		ft_printf("error: the map is too big\n");
 		return ;
+	}
 	game.mlx_win = mlx_new_window(game.mlx, game.x, game.y, "so_long");
 	img = img_creator(game);
 	print_map(game, img);
--- mandatory/game/game_manager.c
+++ mandatory/game/game_manager.c
@@ -6,7 +6,7 @@
 /*   By: hepompid <hepompid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 12:55:14 by hepompid          #+#    #+#             */
-/*   Updated: 2023/10/13 12:43:07 by hepompid         ###   ########.fr       */
+/*   Updated: 2023/10/13 14:30:35 by hepompid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,10 @@ void	game_manager(char **map)
 	game.mlx = mlx_init();
 	map_dimension_finder(map, &game.y, &game.x);
 	if (game.x > 1920 || game.y > 1080)
+	{
+		ft_printf("error: the map is too big\n");
 		return ;
+	}
 	game.mlx_win = mlx_new_window(game.mlx, game.x, game.y, "so_long");
 	img = img_creator(game);
 	print_map(game, img);
