fefb2f79ad405667226af5f2b8a7031255ccec83
Heloise POMPIDOU <heloise.pompidou@outlook.fr>
2023-10-13 13:14:13 +0200

    test gnl

--- utils/get_next_line/get_next_line.h
+++ utils/get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: hepompid <hepompid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/07 10:05:34 by hepompid          #+#    #+#             */
-/*   Updated: 2023/09/06 11:52:16 by hepompid         ###   ########.fr       */
+/*   Updated: 2023/10/13 13:12:51 by hepompid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,9 @@
 
 # include <stdlib.h>
 # include <unistd.h>
+# include "../libft/libft.h"
 
 char	*get_next_line(int fd);
 char	*ft_strjoin_free(char const *s1, char const *s2);
-size_t	ft_strlen(const char *s);
-char	*ft_strdup(const char *s);
 
 #endif
--- utils/get_next_line/get_next_line_utils.c
+++ utils/get_next_line/get_next_line_utils.c
@@ -6,42 +6,11 @@
 /*   By: hepompid <hepompid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/07 10:04:30 by hepompid          #+#    #+#             */
-/*   Updated: 2023/09/06 12:28:31 by hepompid         ###   ########.fr       */
+/*   Updated: 2023/10/13 13:12:34 by hepompid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-size_t	ft_strlen(const char *s)
-{
-	size_t	i;
-
-	i = 0;
-	while (s[i])
-		i++;
-	return (i);
-}
-
-char	*ft_strdup(const char *s)
-{
-	int		i;
-	char	*ptr;
-
-	i = 0;
-	while (s[i])
-		i++;
-	ptr = malloc((i + 1) * sizeof(char));
-	if (!ptr)
-		return (NULL);
-	i = 0;
-	while (s[i])
-	{
-		ptr[i] = s[i];
-		i++;
-	}
-	ptr[i] = '\0';
-	return (ptr);
-}
 
 char	*ft_strjoin_free(char const *s1, char const *s2)
 {
